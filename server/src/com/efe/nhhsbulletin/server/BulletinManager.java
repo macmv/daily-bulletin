d4d2c95ed7e8f37f2d540422dbf88bb3f98d3a59
Macneale <1nemacneale@mhal-sclr210506.seattleschools.org>
2019-09-11 12:24:18 -0700

    First commit; basic http server setup

---
+++ server/src/com/efe/nhhsbulletin/server/BulletinManager.java
@@ -0,0 +1,15 @@
+package com.efe.nhhsbulletin.server;
+
+public class BulletinManager {
+  public BulletinManager() {
+
+  }
+
+  public String getAvailableDates() {
+    return "available dates";
+  }
+
+  public String getDate() {
+    return "date";
+  }
+}
