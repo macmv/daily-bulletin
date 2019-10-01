ca33bc7be54e8bd99d2fff924f79ac4e9a51fb28
Neil Macneale <neil.macneale.v@gmail.com>
2019-10-01 14:51:01 -0700

    Got calendar grid working

--- expo/screens/BulletinScreen.js
+++ expo/screens/BulletinScreen.js
@@ -31,8 +31,8 @@ export default class BulletinScreen extends Component {
         </View>
         <Modal isVisible={this.state.isModalVisible}>
           <View style={styles.linearLayout}>
-            <Text style={styles.text}>I am the popup that we can add the custom calendar to</Text>
-            <Button onPress={this.toggleModal} title="Ok"/>
+            <Calendar/>
+            <Button onPress={this.toggleModal} style={{width: '20%'}} title="Ok"/>
           </View>
         </Modal>
       </View>
@@ -40,21 +40,37 @@ export default class BulletinScreen extends Component {
   }
 }
 
+function Calendar(props) {
+  var calendarRows = [];
+
+  for (var y = 0; y < 5; y++) {
+    var row = [];
+    for (var x = 0; x < 7; x++) {
+      row.push(
+        <View style={styles.calendarNumber} key={y * 7 + x}>
+          <Button disabled={false} title={x.toString()} />
+        </View>
+      );
+    }
+    calendarRows.push(
+      <View style={{flexDirection: 'row'}} key={y}>
+        {row}
+      </View>
+    )
+  }
+
+  console.log(calendarRows);
+
+  return (
+    <View style={{flexDirection: 'column', width: '80%'}}>
+      {calendarRows}
+    </View>
+  )
+}
+
 BulletinScreen.navigationOptions = {
   header: null,
 };
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/development-mode/'
-  );
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
-  );
-}
 
 const styles = StyleSheet.create({
   toolbar: {
@@ -83,88 +99,10 @@ const styles = StyleSheet.create({
   text: {
     color: '#fff',
     fontSize: 20,
-
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+  calendarNumber: {
+    flex: 1,
+    aspectRatio: 1,
+    margin: 5
+  }
 });
