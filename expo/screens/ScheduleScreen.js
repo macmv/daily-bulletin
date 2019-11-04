e8ee07e0d69022e56983b4143e10ea7be60e4d34
Ethan Erickson <coder.efe@gmail.com>
2019-11-04 09:22:54 -0800

    Added switch case for different grades in Schedule Screen

--- expo/screens/ScheduleScreen.js
+++ expo/screens/ScheduleScreen.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import GradeScheduleNavigator from './schedulescreens/GradeScheduleNavigator';
+
+//const GradeIndex = createAppContainer(GradeScheduleNavigator);
 
 export default function ScheduleScreen() {
+  state = {
+    grade: 10
+  };
   return (
-    <ScrollView style={styles.container}>
-      {/**
-       * Go ahead and delete ExpoLinksView and replace it with your content;
-       * we just wanted to provide you with some helpful links.
-       */}
-      <ExpoLinksView />
-    </ScrollView>
+    <View>
+      <GradeScheduleNavigator currentGrade={this.state.grade}/>
+    </View>
   );
 }
 
@@ -24,4 +32,24 @@ const styles = StyleSheet.create({
     paddingTop: 15,
     backgroundColor: '#fff',
   },
+  linearLayoutVertical: {
+    flex: 0,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  /*tab-list {
+    border-bottom: 1px solid #ccc;
+    padding-left: 0;
+  },
+  tab-list-item {
+    display: inline-block;
+    list-style: none;
+    margin-bottom: -1px;
+    padding: 0.5rem 0.75rem;
+  },
+  tab-list-active {
+    background-color: white;
+    border: solid #ccc;
+    border-width: 1px 1px 0 1px;
+  }*/
 });
