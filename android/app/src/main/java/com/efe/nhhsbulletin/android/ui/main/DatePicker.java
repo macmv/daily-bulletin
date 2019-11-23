ac3ba833b1af3a5fdbe23a1f69974915bc42128e
Nathan Hale Robotics Laptop <nhalerobotics@gmail.com>
2019-11-23 15:06:15 -0800

    Added basic clickable text to date picker

--- android/app/src/main/java/com/efe/nhhsbulletin/android/ui/main/DatePicker.java
+++ android/app/src/main/java/com/efe/nhhsbulletin/android/ui/main/DatePicker.java
@@ -3,16 +3,27 @@ package com.efe.nhhsbulletin.android.ui.main;
 import android.app.Dialog;
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.TextView;
 
 import com.efe.nhhsbulletin.android.MainActivity;
 import com.efe.nhhsbulletin.android.R;
 
 public class DatePicker extends Dialog {
 
-  private static final String TAG = MainActivity.class.getSimpleName();
+  private static final String TAG = DatePicker.class.getSimpleName();
 
   public DatePicker(@NonNull Context context) {
     super(context);
     setContentView(R.layout.dialog_date_picker);
+    GridLayout grid = findViewById(R.id.buttonGrid);
+    for (int i = 0; i < 7; i++) {
+      TextView text = new TextView(context);
+      text.setClickable(true);
+      text.setText("" + i);
+      grid.addView(text);
+    }
   }
 }
