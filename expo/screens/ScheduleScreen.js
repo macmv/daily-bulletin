de3f1a534283b8178d3a9f5eeec98ab8fa16f02f
Neil Macneale <neil.macneale.v@gmail.com>
2019-12-18 01:45:37 -0800

    Removed picker, as it was too buggy

--- expo/screens/ScheduleScreen.js
+++ expo/screens/ScheduleScreen.js
@@ -17,47 +17,45 @@ var gradeLoader = new GradeLoader();
 
 export default class ScheduleScreen extends Component {
   state = {
-    grade: 9
+    grade: -1
   };
 
   setGrade = (grade) => {
-    //store user's Grade
     AsyncStorage.setItem("gradekey", JSON.stringify({"grade": grade}), (err,result) => {
-      console.log("error",err,"result",result);
+      if (err) {
+        console.log("Cannot set item, error:", err, ", result:", result);
+      }
     });
-    console.log("Setting grade to: " + grade);
     this.setState({grade: grade});
+    console.log("CALLED setGrade WITH GRADE:", grade);
   }
 
   render() {
+    console.log("Grade in SC is:", this.state.grade);
+    if (this.state.grade != -1) {
+      gradeView = gradeLoader.getView(this.state.grade);
+    } else {
+      gradeView = <View></View>;
+    }
     return (
       <View>
         <NavigationEvents
           onWillFocus={payload => {
-            //get the grade that was saved
             AsyncStorage.getItem("gradekey", (err, result) => {
               if (!err) {
-                //otherwise, load in grade
                 data = JSON.parse(result);
-                if (this.state.grade != data["grade"]) {
-                  this.setState({grade: data["grade"]});
+                grade = data["grade"];
+                if (this.state.grade != grade) {
+                  this.setState({grade: grade});
                 }
               }
+              console.log("Got grade from async", result);
             });
           }}
         />
         <Header title={"Bell Schedule"}>
-          <Picker
-            selectedValue={this.state.grade + ""}
-            style={styles.gradePicker}
-            onValueChange={(itemValue, itemIndex) => this.setGrade(parseInt(itemValue))}>
-            <Picker.Item label="9th Grade" value="9" />
-            <Picker.Item label="10th Grade" value="10" />
-            <Picker.Item label="11th Grade" value="11" />
-            <Picker.Item label="12th Grade" value="12" />
-          </Picker>
         </Header>
-        { gradeLoader.getView(this.state.grade) }
+        { gradeView }
       </View>
     );
   };
